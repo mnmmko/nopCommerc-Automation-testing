168343356811a91329acec4f1b95329e46903d5a
mahmoud nabeh <53482586+mnmmko@users.noreply.github.com>
2022-12-24 14:56:24 +0200

    socend

--- src/main/java/Pages/base_page.java
+++ src/main/java/Pages/base_page.java
@@ -21,4 +21,11 @@ public class base_page {
         jse=(JavascriptExecutor) driver;
         jse.executeScript("scroll(0,250)");
     }
+        public void check_element_is_appear_everyspcifictime(WebDriver driver, WebElement element){
+        FluentWait wait=new FluentWait(driver)
+                .withTimeout(Duration.ofSeconds(5))
+                .pollingEvery(Duration.ofSeconds(1))
+                .ignoring(NoSuchElementException.class);
+        wait.until(ExpectedConditions.invisibilityOf(element));
+    }
 }
